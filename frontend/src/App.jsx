5884b1bada5ce2006ec66c7ca9b34fa162b72b6c
Bushra112 <bushravidyo@gmail.com>
2025-11-26 20:48:32 +0530

    Fix App.jsx imports and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,32 +4,21 @@ import Dashboard from "./pages/Dashboard";
 import Workers from "./pages/Workers";
 import Equipment from "./pages/Equipment";
 import Incidents from "./pages/Incidents";
-import { ToastContainer } from "react-toastify";
 import FloatingChatbot from "./components/FloatingChatbot";
-
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes> ... </Routes>
-      <FloatingChatbot />
-    </>
-  );
-}
-
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
-        <ToastContainer position="top-right" autoClose={2500} />
+
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/workers" element={<Workers />} />
         <Route path="/equipment" element={<Equipment />} />
         <Route path="/incidents" element={<Incidents />} />
       </Routes>
+
+      <FloatingChatbot />
     </BrowserRouter>
   );
 }
-
